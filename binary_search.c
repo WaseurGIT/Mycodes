453c6a2cd83a26f3909e6454df179e074b5bf6b8
Md. Waseur Rahman <148259013+WaseurGIT@users.noreply.github.com>
2024-01-01 17:01:13 +0600

    Add files via upload

---
+++ binary_search.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int a[5],i,n,left,right,middle;
+    printf("Enter sorted array elements:\n");
+    for(i=0;i<5;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Enter a number:");
+    scanf("%d",&n);
+    left=0;
+    right=4;
+
+    while(left<=right){
+    middle=(left+right)/2;
+        if(a[middle]==n){
+            printf("Found at index %d\n",middle);
+            break;
+        }
+        else if(a[middle]<n){
+            left=middle+1;
+        }
+        else{
+            right=middle-1;
+        }
+    }
+    if(left>right){
+        printf("Element not found.");
+    }
+    getch();
+}
