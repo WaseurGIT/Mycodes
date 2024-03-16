a515cde7b884d5be39f0df2a521e4852b8a2f6e2
Md. Waseur Rahman <148259013+WaseurGIT@users.noreply.github.com>
2024-03-16 14:34:52 +0600

    Add files via upload

---
+++ bee_1759 Ho Ho Ho.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,i;
+    printf("Enter an integer number:");
+    scanf("%d",&a);
+    for(i=0;i<a-1;i++){
+        printf("Ho ");
+    }
+    printf("Ho!\n");
+    getch();
+}
